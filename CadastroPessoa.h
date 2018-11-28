1b171e999a78e4960d552a47ebbf94223bb83c7d
ViniciusBiasi <44072865+ViniciusBiasi@users.noreply.github.com>
2018-11-28 20:51:16 -0200

    Add files via upload

---
+++ CadastroPessoa.h
@@ -0,0 +1,11 @@
+#ifndef CADASTROPESSOA_H
+#define CADASTROPESSOA_H
+
+
+class CadastroPessoa
+{
+public:
+    CadastroPessoa();
+};
+
+#endif // CADASTROPESSOA_H
---
+++ PessoaFisica.cpp
@@ -0,0 +1,10 @@
+#include "PessoaFisica.h"
+
+PessoaFisica::PessoaFisica() {
+    // TODO Auto-generated constructor stub
+    peso=altura=0;
+}
+
+PessoaFisica::~PessoaFisica() {
+    // TODO Auto-generated destructor stub
+}
